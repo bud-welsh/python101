052d52eba25b8f6db26645b17df97fd63aac2412
Bud Welsh <budwelsh14@gmail.com>
2020-01-09 21:12:57 -0600

    Chapter 1 commits

--- chapter1.py
+++ chapter1.py
@@ -1,1 +1,8 @@
-print("Hello from Python!")
+Python 3.6.8 (default, Oct  7 2019, 12:59:55) 
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello from Python!")
+Hello from Python!
+>>> print("IDLE is new to me") # but I have used comments in other languages
+IDLE is new to me
+>>> 
