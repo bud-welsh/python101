d5102ff5396ce8bca1b66a760a3fed18e9daf853
Bud Welsh <budwelsh14@gmail.com>
2020-01-09 21:30:58 -0600

    First string commits

---
+++ chapter2.py
@@ -0,0 +1,13 @@
+first_string = 'This is created with single quotes'
+second_string = "This is created with double quotes"
+third_string = '''This is a 
+triple quote string I 
+wrote on multiple lines'''
+fourth_string = """Wrote this string
+on multiple lines with 
+double quotes this time"""
+
+print(first_string)
+print(second_string)
+print(third_string)
+print(fourth_string)
