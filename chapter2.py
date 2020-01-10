0ffd17052016b57a6d22f95b7baedbfa10f47166
Bud Welsh <budwelsh14@gmail.com>
2020-01-10 07:02:50 -0600

    Add quotes and str lessons

--- chapter2.py
+++ chapter2.py
@@ -11,3 +11,16 @@ print(first_string)
 print(second_string)
 print(third_string)
 print(fourth_string)
+
+single_quote = 'This is how we have "double quotes" in a string.'
+double_quote = "An air comma is this string's single quote."
+triple_quote = '''"That's what" - She'''
+triple_quote_again = """"That's what" - She"""
+
+print(single_quote)
+print(double_quote)
+print(triple_quote)
+print(triple_quote_again)
+
+my_number = 123 # This is an integer
+my_string = str(my_number) # This makes the integer a string
