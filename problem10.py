9ddda6e4526041bed716555cb46a4d94b7b7837b
Bud Welsh <budwelsh14@gmail.com>
2020-01-14 06:15:25 -0600

    Problem 10 test solved

--- problem10.py
+++ problem10.py
@@ -2,3 +2,24 @@
 
 # Find the sum of all the primes below two million.
 
+
+# Variables
+stopping_number = 8 # 7 for now, but 2000000 when I know it works
+sum_of_primes = 2 # Start at 2 because it makes the code easier to write
+number = 3 # 3 is the first odd prime number
+denominator = 3
+
+# The Code
+while number < stopping_number:
+    if number / denominator != 1:
+        if number % denominator != 0:
+            denominator += 2
+        else:
+            number += 2
+            denominator = 3
+    else:
+        sum_of_primes = sum_of_primes + number
+        number += 2
+        denominator = 3
+
+print(sum_of_primes)
